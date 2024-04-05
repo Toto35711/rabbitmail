aa58b618f1f5938ec5d14a1474f8474639981474
Toto Budi Trapsilo <toto.trapsilo@nicepay.co.id>
2024-04-05 14:30:54 +0700

    first commit

---
+++ src/main/java/id/tototrapsilo/rabbitmail/RabbitmailApplication.java
@@ -0,0 +1,13 @@
+package id.tototrapsilo.rabbitmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/tototrapsilo/rabbitmail/RabbitmailApplicationTests.java
@@ -0,0 +1,13 @@
+package id.tototrapsilo.rabbitmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
